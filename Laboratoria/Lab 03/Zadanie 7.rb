c032efdfbb46292bae56507003167a92d11c0f5d
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-15 20:24:53 +0100

    Create

---
+++ Laboratoria/Lab 03/Zadanie 7.rb
@@ -0,0 +1,27 @@
+# Klasa drzewa genealogicznego
+class Genealogical_Tree
+  def initialize()
+    @family = Hash[]
+  end
+  def add_person(name, father)
+    @family[name] = father
+  end
+  def print_father(name)
+    puts("Ojciec #{name}a to #{@family[name]}")
+  end
+  def print_grandfather(name)
+    puts("Dziadek #{name}a to #{@family[@family[name]]}")
+  end
+end
+
+# Utworzenie drzewa genealogicznego
+genealogical_tree = Genealogical_Tree.new()
+
+# Wprowadzenie osób do drzewa genealogicznego
+genealogical_tree.add_person("Tomasz", "Waldemar")
+genealogical_tree.add_person("Waldemar", "Stanisław")
+
+# Wyświetlenie ojców i dziadków w drzewie
+genealogical_tree.print_father("Tomasz")
+genealogical_tree.print_father("Waldemar")
+genealogical_tree.print_grandfather("Tomasz")

14450e78760463aebacd9ecd936774f2d4381c52
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-15 15:05:42 +0100

    Create

---
+++ Laboratoria/Lab 03/Zadanie 3.rb
@@ -0,0 +1,5 @@
+subject = "Matematyka"
+marks = [3, 5, 2, 1, 3, 4, 3, 5, 2]
+average_grade = marks.sum / marks.length
+
+puts("Średnia ocen z #{subject} przy wynikach #{marks} wynosi: #{average_grade}!")

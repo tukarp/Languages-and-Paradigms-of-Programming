c2d82392171056ea01ccdd2799c38049cc82a76b
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-15 19:47:38 +0100

    Create

---
+++ Laboratoria/Lab 03/Zadanie 6.rb
@@ -0,0 +1,24 @@
+# Klasa Person
+class Person
+  def initialize(name, surname)
+    @name = name
+    @surname = surname
+  end
+  def imie()
+    puts("Imie osoby to: #{@name}")
+  end
+  def nazwisko()
+    puts("Nazwisko osoby to: #{@surname}")
+  end
+  def to_s()
+    puts("Osoba nazywa się #{@name} #{@surname}")
+  end
+end
+
+# Utworzenie nowej osoby
+person = Person.new("Jan", "Kowalski")
+
+# Wyświetlenie osoby
+person.imie()
+person.nazwisko()
+person.to_s()

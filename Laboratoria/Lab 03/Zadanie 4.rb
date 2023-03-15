cb39465cbc41d5ffbcf24d0b1524feccee036ecd
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-15 14:37:13 +0100

    Create

---
+++ Laboratoria/Lab 03/Zadanie 4.rb
@@ -0,0 +1,24 @@
+# a * x ^ 2 + b * x + c = 0
+
+a = 1
+b = -5
+c = -6
+
+puts("#{a} * x ^ 2 + #{b} * x + #{c} = 0")
+puts("a = #{a}, b = #{b}, c = #{c}")
+
+delta = (b**2) - (4 * a * c)
+delta_square_root = Math.sqrt(delta)
+
+if delta < 0
+    puts("To równanie nie ma rozwiązań!")
+else
+  puts("delta = #{delta}")
+  puts("pierwiastek z delty = #{delta_square_root}")
+  
+  x1 = ((-b - delta_square_root) / (2 * a))
+  x2 = ((-b + delta_square_root) / (2 * a))
+
+  puts("x1 = #{x1}")
+  puts("x2 = #{x2}")
+end

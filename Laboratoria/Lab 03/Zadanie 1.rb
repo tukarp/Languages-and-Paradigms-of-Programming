aebd4866d2ad2828b3fba78314e74fea5d6145fe
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-17 22:17:58 +0100

    Update Zadanie 1.rb

--- Laboratoria/Lab 03/Zadanie 1.rb
+++ Laboratoria/Lab 03/Zadanie 1.rb
@@ -1,53 +1,60 @@
-puts("Fizz Buzz")
+# Fizz Buzz
+def fizz_buzz(min, max)
+  puts("Fizz Buzz")
+  puts("-------------------------")
+  puts("Fizz Buzz w pętli begin: ")
+  puts("-------------------------")
 
-puts("-------------------------")
-puts("Fizz Buzz w pętli begin: ")
-puts("-------------------------")
+  i = min
+  begin
+    if((i % 3 == 0) && (i % 5 == 0))
+      puts("Fizz Buzz")
+    elsif(i % 3 == 0)
+      puts("Fizz")
+    elsif(i % 5 == 0)
+      puts("Buzz")
+    else
+      puts("#{i}")
+    end
+    i += 1
+  end while i < max / 3
 
-i = 1
-begin
-  if((i % 3 == 0) && (i % 5 == 0))
-    puts("Fizz Buzz")
-  elsif(i % 3 == 0)
-    puts("Fizz")
-  elsif(i % 5 == 0)
-    puts("Buzz")
-  else
-    puts("#{i}")
+  puts("-------------------------")
+  puts("Fizz Buzz w pętli while: ")
+  puts("-------------------------")
+
+  while i < 2 * max / 3
+    if((i % 3 == 0) && (i % 5 == 0))
+      puts("Fizz Buzz")
+    elsif(i % 3 == 0)
+      puts("Fizz")
+    elsif(i % 5 == 0)
+      puts("Buzz")
+    else
+      puts("#{i}")
+    end
+    i += 1
   end
-  i += 1
-end while i < 33
 
-puts("-------------------------")
-puts("Fizz Buzz w pętli while: ")
-puts("-------------------------")
-
-while i < 66
-  if((i % 3 == 0) && (i % 5 == 0))
-    puts("Fizz Buzz")
-  elsif(i % 3 == 0)
-    puts("Fizz")
-  elsif(i % 5 == 0)
-    puts("Buzz")
-  else
-    puts("#{i}")
-  end
-  i += 1
-end
-
-puts("-------------------------")
-puts("Fizz Buzz w pętli until: ")
-puts("-------------------------")
+  puts("-------------------------")
+  puts("Fizz Buzz w pętli until: ")
+  puts("-------------------------")
   
-until i > 100
-  if((i % 3 == 0) && (i % 5 == 0))
-    puts("Fizz Buzz")
-  elsif(i % 3 == 0)
-    puts("Fizz")
-  elsif(i % 5 == 0)
-    puts("Buzz")
-  else
-    puts("#{i}")
+  until i > max
+    if((i % 3 == 0) && (i % 5 == 0))
+      puts("Fizz Buzz")
+    elsif(i % 3 == 0)
+      puts("Fizz")
+    elsif(i % 5 == 0)
+      puts("Buzz")
+    else
+      puts("#{i}")
+    end
+    i += 1
   end
-  i += 1
 end
+
+# main
+min = 1
+max = 100
+fizz_buzz(min, max)

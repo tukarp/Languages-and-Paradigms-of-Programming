2829a73ad33b05ea6b19876de05cca803e2dcfa9
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-03-17 22:20:44 +0100

    Update Zadanie 3.rb

--- Laboratoria/Lab 03/Zadanie 3.rb
+++ Laboratoria/Lab 03/Zadanie 3.rb
@@ -1,8 +1,10 @@
+# Obliczenie średniej ocen ucznia
 def student_average_grade(subject, marks)
   average_grade = marks.sum / marks.length
   puts("Średnia ocen z #{subject} przy wynikach #{marks} wynosi: #{average_grade}!")
 end
 
+# main
 subject = "Matematyka"
 marks = [3, 5, 2, 1, 3, 4, 3, 5, 2]
 student_average_grade(subject, marks)

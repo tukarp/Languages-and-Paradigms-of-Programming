6583e195911e67a7f3c993b924984eb36431b19d
Tomasz Wnuk <114019481+tukarp@users.noreply.github.com>
2023-06-02 00:56:10 +0200

    Update Zadanie 05.pl

--- Laboratoria/Lab 01/Zadanie 05.pl
+++ Laboratoria/Lab 01/Zadanie 05.pl
@@ -1,5 +1,10 @@
+% Definicja ojców podanych osów
 ojciec(jan, tomasz).
 ojciec(tomasz, mikołaj).
 ojciec(tomasz, jakub).
+
+% Definicja funkcji znajdującej dziadka od podanej osoby
 dziadek(X, Z) :- ojciec(X, Y), ojciec(Y, Z).
+
+% Znalezienie dziadka od podanej osoby
 ? - dziadek(X, mikołaj).
